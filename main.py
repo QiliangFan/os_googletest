556be1644e3e1a1fd8a6a6e1fa245817e8018784
fanqiliang <SENSETIME\fanqiliang@cn1514001447u.domain.sensetime.com>
2020-10-14 19:02:56 +0800

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+from test_google import  TestGoogle
+
+
+def main():
+    test_case = TestGoogle()
+    test_case.setup_method()
+    test_case.test_google()
+    test_case.teardown_method()
+
+
+if __name__ == "__main__":
+    main()
